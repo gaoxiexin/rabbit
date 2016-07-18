7f81cc18e24bf7a4dc63ab4de541e362374c7754
gaoxiexin <godkill_er@126.com>
2016-07-18 17:00:34 +0800

    新描述

--- rabbitmq/src/main/java/com/gxx/rabbitmq/entity/People.java
+++ rabbitmq/src/main/java/com/gxx/rabbitmq/entity/People.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 @SuppressWarnings("serial")  
-public class People implements Serializable{  
+public class People implements Serializable{
+    //id标识
     private int id;  
     private String name;  
     private boolean male;  
